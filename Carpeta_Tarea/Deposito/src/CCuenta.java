13dfd896ada8e8a4cba2e3b6d8b92e722838925f
Pol Bonet <pbonetimk@fpvirtualaragon.es>
2025-03-21 11:35:22 +0100

    Comentarios Javadoc

--- Carpeta_Tarea/Deposito/src/CCuenta.java
+++ Carpeta_Tarea/Deposito/src/CCuenta.java
@@ -1,47 +1,109 @@
+
+/**
+ *
+ * @author pobom
+ *  La clase CCuenta representa una cuenta bancaria con operaciones básicas como ingresar y retirar dinero.
+ * 
+ */
 public class CCuenta {
-
-
+    
+    
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
-
+    
+      /**
+     * Obtiene el nombre del titular de la cuenta.
+     * 
+     * @return El nombre del titular.
+     */
     public String getNombre() {
         return nombre;
     }
 
+      /**
+     * Establece el nombre del titular de la cuenta.
+     * 
+     * @param nombre El nombre del titular.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    
+    /**
+     * Obtiene el número de cuenta.
+     * 
+     * @return El número de cuenta.
+     */
     public String getCuenta() {
         return cuenta;
     }
 
+    /**
+     * Establece el número de cuenta.
+     * 
+     * @param cuenta El número de cuenta.
+     */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
+     /**
+     * Obtiene el saldo actual de la cuenta.
+     * 
+     * @return El saldo actual de la cuenta.
+     */
     public double getSaldo() {
         return saldo;
     }
 
+    /**
+     * Establece el saldo de la cuenta.
+     * 
+     * @param saldo El saldo a establecer.
+     */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
+      /**
+     * Obtiene el tipo de interés asociado a la cuenta.
+     * 
+     * @return El tipo de interés de la cuenta.
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
+    
+    /**
+     * Establece el tipo de interés de la cuenta.
+     * 
+     * @param tipoInterés El tipo de interés a establecer.
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
 
+      /**
+     * Constructor vacío de la clase CCuenta.
+     * Este constructor crea una cuenta sin inicializar los valores.
+     */
     public CCuenta()
     {
     }
 
+    
+    /**
+     * Constructor de la clase CCuenta con parámetros para inicializar la cuenta.
+     * 
+     * @param nom El nombre del titular de la cuenta.
+     * @param cue El número de cuenta.
+     * @param sal El saldo inicial de la cuenta.
+     * @param tipo El tipo de interés asociado a la cuenta.
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -49,11 +111,24 @@ public class CCuenta {
         saldo=sal;
     }
 
+    
+    /**
+     * Devuelve el saldo actual de la cuenta.
+     * 
+     * @return El saldo de la cuenta.
+     */
     public double estado()
     {
         return saldo;
     }
 
+    
+    /**
+     * Ingresa una cantidad de dinero en la cuenta.
+     * 
+     * @param cantidad La cantidad de dinero a ingresar. Debe ser mayor que 0.
+     * @throws Exception Si la cantidad es negativa.
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -61,6 +136,13 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    
+    /**
+     * Retira una cantidad de dinero de la cuenta.
+     * 
+     * @param cantidad La cantidad de dinero a retirar. Debe ser mayor que 0.
+     * @throws Exception Si la cantidad es negativa o si no hay suficiente saldo.
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
--- Carpeta_Tarea/Deposito/src/Main.java
+++ Carpeta_Tarea/Deposito/src/Main.java
@@ -1,17 +1,39 @@
 
+/**
+ *
+ * @author pobom
+ *
+ * La clase Main contiene el punto de entrada principal para ejecutar la aplicación,
+ * gestionando las operaciones sobre una cuenta bancaria.
+ *
+ */
 public class Main {
 
+    /**
+     * Método principal que inicializa una cuenta bancaria y realiza operaciones
+     * sobre ella.
+     *
+     * @param args Los argumentos de la línea de comandos (no se usan en este
+     * caso).
+     */
     public static void main(String[] args) {
         CCuenta miCuenta;
 
         miCuenta = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
         double saldoActual;
+
         saldoActual = miCuenta.estado();
         System.out.println("El saldo actual es" + saldoActual);
-        
+
         operativa_cuenta(miCuenta, 1);
     }
 
+    /**
+     * Realiza operaciones de retiro e ingreso en la cuenta bancaria.
+     *
+     * @param miCuenta La cuenta sobre la que se realizarán las operaciones.
+     * @param cantidad La cantidad de dinero a retirar o ingresar.
+     */
     public static void operativa_cuenta(CCuenta miCuenta, float cantidad) {
         try {
             miCuenta.retirar(cantidad);
